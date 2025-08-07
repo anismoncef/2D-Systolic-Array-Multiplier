188f1d107a93e09ca2248fa1775298bf823f35bc
Habib AOUINTI <123462058+habibaouinti@users.noreply.github.com>
2025-08-07 11:31:34 +0200

    Add files via upload

--- rtl/systolicArray.sv
+++ rtl/systolicArray.sv
@@ -15,39 +15,36 @@
 // PE[3][0] --> PE[3][1] --> PE[3][2] --> PE[3][3]
 
 `default_nettype none
+`include "./pe.sv"
 
-module systolicArray
-  #(parameter int unsigned N = 4)
+module systolicArray#(
+  parameter BITWIDTH = 4,
+  parameter N = 4,
+  parameter OUTWIDTH = 8
+  )
   ( input  var logic                         i_clk
   , input  var logic                         i_arst
 
   , input  var logic                         i_doProcess
 
-  , input  var logic [N-1:0][(2*N)-2:0][7:0] i_row
-  , input  var logic [N-1:0][(2*N)-2:0][7:0] i_col
+  , input  var logic [N-1:0][BITWIDTH -1:0] i_row
+  , input  var logic [N-1:0][BITWIDTH -1:0] i_col
 
-  , output var logic [N-1:0][N-1:0][31:0]    o_c
+  , output var logic [N-1:0][N-1:0][OUTWIDTH -1:0]    o_c
   );
 
   /* verilator lint_off UNUSED */
   // Variable used to pass data horizontally between PEs in the same row. The
   // output o_a of one PE is connected to the input i_a of the PE to its right.
-  logic [N-1:0][N:0][7:0] rowInterConnect;
-
-  // Variable used to pass data vertically between PEs in the same column. The
-  // output o_b of one PE is connected to the input i_b of the PE below it.
-  logic [N:0][N-1:0][7:0] colInterConnect;
+  wire [N-1:0][N:0][BITWIDTH -1:0] rowInterConnect;
+  wire [N:0][N-1:0][BITWIDTH -1:0] colInterConnect;
   /* verilator lint_off UNUSED */
 
   for (genvar i = 0; i < N; i++) begin: PerDummyRowColInterconnect
 
-    // These are dummy interconnects used to pass data from the row matrices to
-    // the i_a ports of PE in the first col.
     always_comb
       rowInterConnect[i][0] = i_row[i][0];
 
-    // These are dummy interconnects used to pass data  from the col matrices to
-    // the i_b ports of PE in the first row.
     always_comb
       colInterConnect[0][i] = i_col[i][0];
 
