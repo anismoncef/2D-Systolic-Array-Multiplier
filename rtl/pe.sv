47237196d8a87d13994fb91ee81051ac2c4a4c55
Habib AOUINTI <123462058+habibaouinti@users.noreply.github.com>
2025-08-08 12:44:46 +0200

    Add files via upload

--- rtl/pe.sv
+++ rtl/pe.sv
@@ -1,80 +1,37 @@
-// This module implements a Processing Element (PE). It performs an 8-bit
-// multiply-accumulate operation.
+module pe #(
+    parameter BITWIDTH = 4,
+    parameter OUTWIDTH = 2*BITWIDTH
+    ) (
+    input                           clk,
+    input                           compute_en,
+    input                           reset,
+    input                           load_weights,
+    input        [BITWIDTH -1:0]    in_data,    // Input data from top
+    input        [BITWIDTH -1:0]    in_weight,  // Input weight from left
+    output logic [BITWIDTH -1:0]    out_data,   // Output data to bottom
+    output logic [BITWIDTH -1:0]    out_weight, // Output weight to right
+    output logic [OUTWIDTH -1:0]    out_result  // Output accumulated result
+);
+logic res_res ;
+always @(posedge clk) begin
+    if (reset) begin
+        out_data    <= 0;
+        out_weight  <= 0;
+        out_result  <= 0;
+        res_res     <= 0;
+    end 
+    else 
+    begin
+        if (load_weights) 
+        begin
+            out_result  <= 0;
+        end else if (compute_en) begin
 
-`default_nettype none
+            out_data    <= in_data;
+            out_weight  <= in_weight;
+            out_result  <= out_result + (in_data * in_weight);
 
-`ifndef PE_SV
-`define PE_SV
-module pe#(
-  parameter BITWIDTH = 4,
-  parameter N = 4,
-  parameter OUTWIDTH = 8
-  )
-  ( input  var logic        i_clk
-  , input  var logic        i_arst
-
-  , input  var logic        i_doProcess
-
-  , input  var logic [BITWIDTH -1:0]  i_a
-  , input  var logic [BITWIDTH -1:0]  i_b
-
-  , output var logic [BITWIDTH -1:0]  o_a
-  , output var logic [BITWIDTH -1:0]  o_b
-  , output var logic [OUTWIDTH -1:0]  o_y
-  );
-
-  // {{{ MAC
-
-  logic [OUTWIDTH -1:0] mult;
-
-  always_comb
-    mult = i_a*i_b;
-
-  logic [OUTWIDTH -1:0] mac_d, mac_q;
-
-  always_ff @(posedge i_clk, posedge i_arst)
-    if (i_arst)
-      mac_q <= '0;
-    else
-      mac_q <= mac_d;
-
-  always_comb
-    mac_d = (i_doProcess) ? mac_q + mult : '0;
-
-  always_comb
-    o_y = mac_q;
-
-  // }}} MAC
-
-  // {{{ Register inputs and assign them to outputs
-
-  logic [BITWIDTH -1:0] a_q, b_q;
-
-  always_ff @(posedge i_clk, posedge i_arst)
-    if (i_arst)
-      a_q <= '0;
-    else if (i_doProcess)
-      a_q <= i_a;
-    else
-      a_q <= a_q;
-
-  always_ff @(posedge i_clk, posedge i_arst)
-    if (i_arst)
-      b_q <= '0;
-    else if (i_doProcess)
-      b_q <= i_b;
-    else
-      b_q <= b_q;
-
-  always_comb
-    o_a = a_q;
-
-  always_comb
-    o_b = b_q;
-
-  // }}} Register inputs and assign them to outputs
-
+        end
+    end
+end
 endmodule
-`endif
-
-`resetall
